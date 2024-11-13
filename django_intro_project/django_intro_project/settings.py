e7df2c8b47edbd458b34e7c7b043790bb714bf66
maiaa04 <majaludwinska@gmail.com>
2024-11-13 09:42:29 +0100

    set up dbconnection

--- django_intro_project/django_intro_project/settings.py
+++ django_intro_project/django_intro_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'intro_app',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,14 @@ WSGI_APPLICATION = 'django_intro_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'se_django_db',
+		'USER': 'admin',
+		'PASSWORD': 'Adgjl@123',
+		'HOST': 'localhost',
+		'PORT': '5432',
+	}
 }
 
 
