13fbe869d000801117be2f824c3bc5cf80fcdc37
maiaa04 <majaludwinska@gmail.com>
2024-11-13 09:57:49 +0100

    added product model

--- django_intro_project/intro_app/models.py
+++ django_intro_project/intro_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    price = models.FloatField()
+    available = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
