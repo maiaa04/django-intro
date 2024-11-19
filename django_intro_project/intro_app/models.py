fbd4f221c2f1597c6a3d989999d09968dc4f0f87
maiaa04 <majaludwinska@gmail.com>
2024-11-19 17:26:26 +0100

    added constraints and validation to models

--- django_intro_project/intro_app/models.py
+++ django_intro_project/intro_app/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
-
+def positive(value):
+    if value <= 0:
+        raise ValidationError('Value must be positive')
+    
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
-    price = models.FloatField()
+    price = models.DecimalField(decimal_places=2, validators=[positive])
     available = models.BooleanField(default=False)
 
     def __str__(self):
@@ -13,7 +17,7 @@ class Product(models.Model):
     
 class Customer(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     address = models.TextField()
 
     def __str__(self):
@@ -26,14 +30,19 @@ class Order(models.Model):
     date = models.DateField(auto_now_add=True)
 
     ORDER_STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('in_progress', 'In Progress'),
-        ('shipped', 'Shipped'),
-        ('delivered', 'Delivered'),
-        ('cancelled', 'Cancelled'),
+        ('new', 'New'),
+        ('in_process', 'In Process'),
+        ('sent', 'Sent'),
+        ('completed', 'Completed'),
     ]
 
-    status = models.CharField(max_length=20, choices=ORDER_STATUS_CHOICES, default='pending')
+    status = models.CharField(max_length=20, choices=ORDER_STATUS_CHOICES, default='new')
 
     def __str__(self):
         return "order id:" + str(self.id) + " status:" + self.status
+    
+    def order_price(self):
+        return sum(p.price for p in self.products.all())
+    
+    def fullfillable(self):
+        return all(p.available for p in self.products.all())
