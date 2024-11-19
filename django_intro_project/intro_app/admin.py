5809699d7028ef1eef4c5ccbf2c42213e9aa8d59
maiaa04 <majaludwinska@gmail.com>
2024-11-19 17:02:16 +0100

    registered models in admin.py

--- django_intro_project/intro_app/admin.py
+++ django_intro_project/intro_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Customer, Order
+
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Order)
 
 # Register your models here.
