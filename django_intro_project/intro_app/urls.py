f9353476f9b3f47fab53024fec66b361ec77bf01
maiaa04 <majaludwinska@gmail.com>
2024-11-20 10:26:59 +0100

    added product methods and url pattern - lab 6, part 1

--- django_intro_project/intro_app/urls.py
+++ django_intro_project/intro_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .views import hello_world
+from .views import product_list, product_detail
 
 urlpatterns = [
-path('hello/', hello_world, name='hello_world'),
+    path('hello/', hello_world, name='hello_world'),
+]
+urlpatterns = [
+    path('api/products/', product_list,
+        name='product_list'),
+    path('api/products/<int:product_id>/', product_detail,
+        name='product_detail'),
 ]
--- django_intro_project/intro_app/views.py
+++ django_intro_project/intro_app/views.py
@@ -1,7 +1,49 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Product
+from decimal import Decimal
 
 # Create your views here.
 from django.http import HttpResponse
 
 def hello_world(request):
   return HttpResponse("Hello, World!")
+
+@csrf_exempt
+def product_list(request):
+  if request.method == 'GET':
+    products = list(Product.objects.values('id',
+                                           'name', 'price', 'available'))
+    return JsonResponse(products, safe=False)
+  elif request.method == 'POST':
+    data = json.loads(request.body)
+  name = data.get('name')
+  price = data.get('price')
+  available = data.get('available')
+  product = Product(name=name,
+    price=Decimal(str(price)), available=available)
+  product.full_clean()
+  product.save()
+  return JsonResponse({
+    'id': product.id,
+    'name': product.name,
+    'price': float(product.price),
+    'available': product.available
+    },
+    status=201
+  )
+
+@csrf_exempt
+def product_detail(request, product_id):
+  if request.method == 'GET':
+    product = Product.objects.get(id=product_id)
+  return JsonResponse({
+    'id': product.id,
+    'name': product.name,
+    'price': float(product.price),
+    'available': product.available
+    }
+  )
+
