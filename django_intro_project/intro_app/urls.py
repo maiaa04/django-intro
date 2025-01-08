3e4348cbb8d5f072ea9afe2217d7c9642560917e
maiaa04 <majaludwinska@gmail.com>
2025-01-08 09:58:55 +0100

    finished Part 1 Lab 9

--- django_intro_project/intro_app/urls.py
+++ django_intro_project/intro_app/urls.py
@@ -6,13 +6,28 @@ from .views import ProductViewSet
 from .views import CustomerViewSet
 from .views import OrderViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'customers', CustomerViewSet, basename='customer')
 router.register(r'orders', OrderViewSet, basename='order')
-                
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Software engineering lab",
+        default_version="v1",
+        description="API documentation for the lab",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+    authentication_classes=[],
+)
+
+
 urlpatterns = [
     path('hello/', hello_world, name='hello_world'),
 ]
@@ -21,4 +36,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('swagger/', schema_view.with_ui('swagger',
+                                         cache_timeout=0), name='schema-swagger-ui'),
 ]
+
